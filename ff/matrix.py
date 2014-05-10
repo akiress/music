06aff38e5510f0fc5b0bc0a4b7b6f9f06297ec77
Benjamin Guitreau <bguitr1@tigers.lsu.edu>
2014-05-10 15:53:27 -0500

    Adding final project sans paper for now.

---
+++ ff/matrix.py
@@ -0,0 +1,69 @@
+import RPi.GPIO as GPIO
+import os
+import time
+
+class ButtonMatrix():
+    BUTTON_MATRIX = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+    ROW = [22,27,18,17]
+    COLUMN = [10,9,11,23]
+    
+    def __init__(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+    
+    def buttonPressed(self):
+        for j in range(len(self.COLUMN)):
+            GPIO.setup(self.COLUMN[j], GPIO.OUT)
+            GPIO.output(self.COLUMN[j], GPIO.LOW)
+        
+        for i in range(len(self.ROW)):
+            GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+        rowVal = -1
+        for i in range(len(self.ROW)):
+            tmpRead = GPIO.input(self.ROW[i])
+            if tmpRead == 0:
+                rowVal = i
+                
+        if rowVal < 0 or rowVal > 3:
+            self.exit()
+            return
+        
+        for j in range(len(self.COLUMN)):
+                GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        
+        GPIO.setup(self.ROW[rowVal], GPIO.OUT)
+        GPIO.output(self.ROW[rowVal], GPIO.HIGH)
+
+        colVal = -1
+        for j in range(len(self.COLUMN)):
+            tmpRead = GPIO.input(self.COLUMN[j])
+            if tmpRead == 1:
+                colVal=j
+                
+        if colVal < 0 or colVal > 3:
+            self.exit()
+            return
+
+        self.exit()
+        return self.BUTTON_MATRIX[rowVal][colVal]
+        
+    def exit(self):
+        for i in range(len(self.ROW)):
+                GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+        for j in range(len(self.COLUMN)):
+                GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+if __name__ == '__main__':
+    key = ButtonMatrix()
+    temp = None
+    while True:
+        button = key.buttonPressed()
+        if button is not None:
+            if button == 15 or button == 16 or button == 13:
+                os.system("echo ' %d;' | pdsend 3000" % (button))
+                time.sleep(0.05)
+            else:
+                os.system("echo ' %d;' | pdsend 3000" % (button))
+                time.sleep(0.25)
+        button = None
